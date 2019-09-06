c2df685e2517088477df83940d1d5ad64dd7fb06
M Fachmi Firmansyah <firmansyahfachmi5@gmail.com>
2019-09-06 23:31:04 +0700

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, {Fragment} from "react";
+// import Route from 'react-router-dom'
 import "./App.css";
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        Asas
-      </header>
-    </div>
+    <Fragment>
+
+    </Fragment>
   );
 };
 
