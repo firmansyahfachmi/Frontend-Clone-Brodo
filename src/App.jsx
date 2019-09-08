39c6b986a414db2bc3db3103efd2090751b413d7
Galih Mahendra Wicaksono 62 <mahendragalih26@gmail.com>
2019-09-08 15:57:37 +0700

    fix router and page per component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
 import React from "react";
 import Footer from "./components/layout/footer";
-import Card from "./components/card/Card";
-// import Route from 'react-router-dom'
-
 import Header from "./components/layout/header.jsx";
-import Carousel from "./components/carousel/Carousel";
+import HomePage from "./components/page/homePage";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import "./App.css";
-// import Header from "./components/layout/header";
 
 const App = () => {
   return (
     <Router>
       <Header />
-      <Carousel type={"main"} />
-      <Card type={"card"} />
-      <Carousel type={"main1"} />
-      <Card type={"card1"} />
-
+      <Route
+        path="/"
+        exact
+        render={() => {
+          return <HomePage />;
+        }}
+      />
       <Footer />
     </Router>
   );
