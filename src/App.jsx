bcab2d4842a58f80cdb91e2a604212b177517d95
Galih Mahendra Wicaksono 62 <mahendragalih26@gmail.com>
2019-09-08 16:35:22 +0700

    add detail component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-   
+
 import Header from "./components/layout/header.jsx";
 import Footer from "./components/layout/footer";
 import HomePage from "./components/page/homePage";
 import Collection from "./components/page/collection.jsx";
+import Detail from "./components/page/detail.jsx";
 
 import "./App.css";
 
@@ -19,12 +20,20 @@ const App = () => {
           return <HomePage />;
         }}
       />
-      <Route path="/collection"
-          exact
-          render={() => {
-            return <Collection/>
-          }}
-        />
+      <Route
+        path="/collection"
+        exact
+        render={() => {
+          return <Collection />;
+        }}
+      />
+      <Route
+        path="/product"
+        exact
+        render={() => {
+          return <Detail />;
+        }}
+      />
       <Footer />
     </Router>
   );
---
+++ src/components/detail/Detail.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+const detailBar = () => {
+  return (
+    <Fragment>
+      <div></div>
+    </Fragment>
+  );
+};
+
+export default detailBar;
